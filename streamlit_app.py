14bfc08cd3d65d3977a6d8c40fefbbc15dbf77e3
prozip <quachkimson2000@gmail.com>
2022-04-02 08:25:34 +0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ import os
 st.title('Music Generation with RNN/LSTM')
 st.write(tf.__version__)
 os.mkdir('test')
-st.write(os.listdir)
+st.write(os.listdir())
 subprocess.run([f"{sys.executable}", "predict.py"])
