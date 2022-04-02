2362d9ddef3da3d2ec10141aae46d85c173f20bc
prozip <quachkimson2000@gmail.com>
2022-04-02 08:17:08 +0700

    change to subprocess

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import os
+import subprocess
 import tensorflow as tf
 
 st.title('Music Generation with RNN/LSTM')
 st.write(tf.__version__)
-os.system('python3 predict.py')
+subprocess.run(["python", "predict.py"])
