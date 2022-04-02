15beca6a76596538b59ab76d5095651b808c309c
prozip <quachkimson2000@gmail.com>
2022-04-02 08:25:10 +0700

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import subprocess, sys
 import tensorflow as tf
+import os
 
 st.title('Music Generation with RNN/LSTM')
 st.write(tf.__version__)
+os.mkdir('test')
+st.write(os.listdir)
 subprocess.run([f"{sys.executable}", "predict.py"])
